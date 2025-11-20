fcfc4275c4d2fe404e70c80facb0df3e96a40a17
ramkumar8995 <ramkumar.bypilla@gmail.com>
2025-11-20 11:19:00 +0000

    Update ride form logic

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ class Ride(models.Model):
     ride_from = models.CharField(max_length=100)
     ride_to = models.CharField(max_length=100)
     payment_type = models.CharField(max_length=20)
-    ride_type = models.CharField(max_length=20)
+    ride_type = models.CharField(max_length=20, default='app')
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     tips = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
